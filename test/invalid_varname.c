67edf277468e5254ffcbb1fab5eeab1d99eaa18f
RuleOfSix <github@ruleofsix.ca>
2024-10-28 14:00:44 -0700

    Added invalid varname test

---
+++ test/invalid_varname.c
@@ -0,0 +1,29 @@
+#include <parse.h>
+#include <project_info.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+int32_t main() {
+	token input_tokens[] = {
+		{"~ATH", 1},
+		{"(", 1},
+		{"NULL", 1},
+		{")", 1},
+		{"{", 1},
+		{"}", 2},
+		{"EXECUTE", 2},
+		{"(", 2},
+		{"NULL", 2},
+		{")", 2},
+		{";", 2}
+	};
+	token_list input = {input_tokens, 12};
+
+	ast* output = parse(&input);
+	fprintf(stderr, "Test failed: parser did not throw a syntax error when given incorrect input.\n");
+	return 0;
+}
+
