bb66a04eb5b6f112a75024f4059894faf9ee12c5
RuleOfSix <accounts@ruleofsix.ca>
2024-10-28 19:22:25 -0700

    Added new test, fixed parsing bugs related to bounds checking.

---
+++ test/unfinished_grave.c
@@ -0,0 +1,20 @@
+#include <parse.h>
+#include <project_info.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+
+int32_t main() {
+	token input_tokens[] = {
+		{"~ATH", 1},
+		{"(", 1},
+	};
+	token_list input = {input_tokens, 2};
+
+	parse(&input);
+	fprintf(stderr, "Test failed: parser did not throw a syntax error when given incorrect input.\n");
+	return 0;
+}
+
