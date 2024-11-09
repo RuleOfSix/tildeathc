16f3e435701b1de99dc5f868c4cbd8d1c79bc93b
RuleOfSix <github@ruleofsix.ca>
2024-11-09 15:45:21 -0800

    Updated util.c and updated parse.c to use util.c

--- src/util.c
+++ src/util.c
@@ -2,10 +2,12 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+#include <nullcheck.h>
 
 char* util_newstr(const char* str) {
 	int64_t len = strlen(str);
 	char* out = malloc((len + 1) * sizeof(char));
+	MALLOC_NULL_CHECK(out);
 	memcpy(out, str, len + 1);
 	return out;
 }
