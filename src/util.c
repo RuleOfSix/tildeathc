d0f6a8caf2c87954416da71f1d3ecd019b3b93b5
RuleOfSix <github@ruleofsix.ca>
2024-12-22 13:03:02 -0800

    Implemented variable scope

--- src/util.c
+++ src/util.c
@@ -46,3 +46,56 @@ int32_t append_to_strarray(struct strarray* a, char* new_entry, bool duplicate) 
 	a->len++;
 	return 0;
 }
+
+int32_t append_to_ptrarray(struct ptrarray* a, void* new_entry) {
+	if (a == NULL) {
+		return 1;
+	}
+	if (a->array == NULL) {
+		a->cap = 2;
+		a->len = 0;
+		a->array = malloc(a->cap * sizeof(*(a->array)));
+		MALLOC_NULL_CHECK(a->array);
+	}
+	if (a->len >= a->cap) {
+		a->cap *= 2;
+		a->array = realloc(a->array, a->cap * sizeof(*(a->array)));
+		MALLOC_NULL_CHECK(realloc);
+	}
+	a->array[a->len] = new_entry;
+	a->len++;
+	return 0;
+}
+
+struct strarray* dup_strarray(struct strarray* a) {
+	struct strarray* output = malloc(sizeof(*output));
+	MALLOC_NULL_CHECK(output);
+	output->len = a->len;
+	output->cap = a->cap;
+	output->array = malloc(sizeof(*(output->array)) * output->cap);
+	MALLOC_NULL_CHECK(output->array);
+	for (int64_t i = 0; i < output->len; i++) {
+		output->array[i] = util_strdup(a->array[i]);
+	}
+	return output;
+}
+
+struct ptrarray* dup_ptrarray(struct ptrarray* a) {
+	struct ptrarray* output = malloc(sizeof(*output));
+	MALLOC_NULL_CHECK(output);
+	output->len = a->len;
+	output->cap = a->cap;
+	output->array = malloc(sizeof(*(output->array)) * output->cap);
+	MALLOC_NULL_CHECK(output->array);
+	for (int64_t i = 0; i < output->len; i++) {
+		output->array[i] = a->array[i];
+	}
+	return output;
+}
+
+void free_strarray(struct strarray* a) {
+	for (int64_t i = 0; i < a->len; i++) {
+		free(a->array[i]);
+	}
+	free(a->array);
+}
